af5734b2d00dd98432c2b16658ca6220bad886fc
leonardo.orejuela@gmail.com <leonardo.orejuela@gmail.com>
2022-02-16 10:47:33 -0500

    get track by id and modules by track

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,7 +4,7 @@ const typeDefs = gql`
     type Query {
         "Query to get tracks array for the homepage grid"
         tracksForHome: [Track!]!
-        tracksForHomeFetch: [Track!]!
+        track(id: ID!): Track
     }
 
     "A track is a group of modules that teaches about a specific topic"
@@ -20,6 +20,12 @@ const typeDefs = gql`
         length: Int
         "the number of modules this track contains"
         modulesCount: Int
+        "the track's complete description, can be in Markdown format"
+        description: String
+        "the number of times a track has been viewed"
+        numberOfViews: Int
+        "the track's complete array of Modules"
+        modules: [Module!]!
     }
 
     "Author of a complete Track"
@@ -29,6 +35,15 @@ const typeDefs = gql`
         name: String!
         "Author's profile picture url"
         photo: String
+    }
+
+    "A Module is a single unit of teaching. Multiple Modules compose a Track"
+    type Module {
+        id: ID!
+        "the Module's title"
+        title: String!
+        "the Module's length in minutes"
+        length: Int
     }
 `;
 module.exports = typeDefs;
