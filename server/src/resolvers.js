35a938d41fd4bc003ed2cb52fbd9eda9dd57a189
leonardo.orejuela@gmail.com <leonardo.orejuela@gmail.com>
2022-02-15 20:51:37 -0500

    implement restdatasource, resolver, query livedata

---
+++ server/src/resolvers.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+
+const resolvers = {
+    Query: {
+      // get all tracks, will be used to populate 
+      // the homepage grid of our web client
+      tracksForHome: (_, __, {dataSources}) => {
+        return dataSources.trackAPI.getTracksForHome();
+      },
+    },
+    Track: {
+      author: ({authorId}, _, {dataSources}) => {
+        return dataSources.trackAPI.getAuthor(authorId);
+      },
+    },
+};
+
+module.exports = resolvers;
