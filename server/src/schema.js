2d8544c393713a7d9a50d5744adc983cb5d6fe15
leonardo.orejuela@gmail.com <leonardo.orejuela@gmail.com>
2022-03-03 10:25:27 -0500

    add mutation and response object

--- server/src/schema.js
+++ server/src/schema.js
@@ -28,6 +28,21 @@ const typeDefs = gql`
         modules: [Module!]!
     }
 
+    type Mutation {
+        incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+    }
+    
+    type IncrementTrackViewsResponse {
+        "Similar to HTTP status code, represents the status of the mutation"
+        code: Int!
+        "Indicates whether the mutation was successful"
+        success: Boolean!
+        "Human-readable message for the UI"
+        message: String!
+        "Newly updated track after a successful mutation"
+        track: Track
+    }    
+
     "Author of a complete Track"
     type Author {
         id: ID!
